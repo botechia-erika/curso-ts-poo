bb7394f352f9d6758555b39df9b54a3c5e9f1acb
Guilherme Vieira <klauser@hotmail.com.br>
2023-07-29 12:58:19 -0300

    feat: add missing exercise

---
+++ src/07-exercicios/exercicio-6.ts
@@ -0,0 +1,11 @@
+/*
+  Exercício 6:
+  Crie uma função genérica chamada trocarElementos que receba um array (array 
+  genérico) e dois índices como parâmetros, e troque os elementos dos índices 
+  fornecidos no array. Certifique-se de utilizar tipos genéricos para que a 
+  função funcione com diferentes tipos de arrays.
+
+  Lembre-se de utilizar a sintaxe do TypeScript, incluindo a definição de tipos 
+  (interfaces, classes, enums, tipos genéricos) e a anotação de tipos em 
+  variáveis e funções.
+*/
